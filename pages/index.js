14631bf37e91c9bcc0fefc88206c77ea81a1b526
iandlocam <iand.locam@gmail.com>
2026-07-02 22:01:17 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -847,7 +847,7 @@ export default function Home() {
       "Formal / jardín elegante",
       "Te sugerimos telas frescas y cómodas. Evita tacones muy delgados por el terreno."
       "Ayúdanos a que la novia sea la única de blanco. Les pedimos no usar blanco ni tonos similares. ¡Gracias por ser parte de este día!."
-  "Les sugerimos los siguientes colores 🤍."
+  "Les sugerimos los siguientes colores."
     ],
   };
 
