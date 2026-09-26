5271d8dd44c5e8178898cdced1ef965fc224d98e
iandlocam <iand.locam@gmail.com>
2026-09-25 22:51:35 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2230,7 +2230,6 @@ export default function Home() {
 
             {/* Nota final */}
             <div style={{ marginTop: 30, fontSize: "clamp(1.3rem, 3.5vw, 1.5rem)", color: COLORS.textMedium, textAlign: "center", fontWeight: 700}}>
-              *No se permiten menores de 16 años.<br />
               Invitación personal.<br />
               Sin acompañantes adicionales.*
             </div>
